f50024c2482c82fe47da275d21c6ff50af04b060
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-01-03 22:55:40 -0300

    Revert "Temporarily switch to GDB 16.3"
    This reverts commit acd825b0920adac2ef112e49cf134339742fb693.

--- build.sh
+++ build.sh
@@ -161,7 +161,7 @@ fi
 
 if ! [ -f "${gdb_tarball}" ]; then
 	curl \
-		--url 'https://ftp.gnu.org/gnu/gdb/gdb-16.3.tar.xz' \
+		--url 'https://github.com/AmanoTeam/binutils-snapshots/releases/latest/download/gdb.tar.xz' \
 		--retry '30' \
 		--retry-all-errors \
 		--retry-delay '0' \
@@ -175,9 +175,7 @@ if ! [ -f "${gdb_tarball}" ]; then
 		--extract \
 		--file="${gdb_tarball}"
 	
-	# mv "${install_prefix}" "${gdb_directory}"
-	
-	mv '/tmp/gdb-16.3' "${gdb_directory}"
+	mv "${install_prefix}" "${gdb_directory}"
 	
 	echo 'UNSUPPORTED=1' >> "${gdb_directory}/gdbserver/configure.srv"
 	
